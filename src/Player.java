f46bcdc7d07ff7d62956edcf80148c4926d070a4
Heli Hård Källoff <vierge.hard@gmail.com>
2024-09-18 17:45:16 +0200

    add name variable

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
 public class Player {
+  private String name;
   private String marker;
 
   // empty constructor
@@ -6,8 +7,13 @@ public class Player {
 
   }
 
-  public Player(String marker) {
+  public Player(String name, String marker) {
+    this.name = name;
     this.marker = marker;
+  }
+
+  public String getName() {
+    return name;
   }
 
   public String getMarker() {
