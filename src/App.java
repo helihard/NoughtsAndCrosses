63246d9ca60e19729d16d57bdec8bd6c157fa659
Heli Hård Källoff <vierge.hard@gmail.com>
2024-09-18 17:06:04 +0200

    add method to update board

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Board board = new Board();
+        board.updateBoard(9, "X");
+        board.updateBoard(9, "O");
+        board.updateBoard(8, "X");
     }
 }
--- src/Board.java
+++ src/Board.java
@@ -10,10 +10,21 @@ public class Board {
   }
 
   public void printBoard() {
-    System.out.println(" " + this.squares[0] + " | " + this.squares[1] + " | " + this.squares[2]);
+    System.out.println("\n " + this.squares[0] + " | " + this.squares[1] + " | " + this.squares[2]);
     System.out.println("–––+–––+–––");
     System.out.println(" " + this.squares[3] + " | " + this.squares[4] + " | " + this.squares[5]);
     System.out.println("–––+–––+–––");
-    System.out.println(" " + this.squares[6] + " | " + this.squares[7] + " | " + this.squares[8]);
+    System.out.println(" " + this.squares[6] + " | " + this.squares[7] + " | " + this.squares[8] + "\n");
+  }
+
+  // error handling and input validation to be added
+  public void updateBoard(int squareNumber, String letter) {
+    int index = squareNumber -1;
+    if (!this.squares[index].equals("-")) {
+      System.out.println("Square already claimed by player");
+    } else {
+      this.squares[index] = letter;
+      printBoard();
+    }
   }
 }
